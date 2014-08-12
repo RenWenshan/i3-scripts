4fb15dcfdde92fc9f1f27ff5f2091abb2f5504d0
Ren Wenshan <wenshan@trikeapps.com>
2014-08-12 21:40:16 +1000

    swap-workspace: only swap when there are two screens

--- swap-workspace.py
+++ swap-workspace.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python2.7
+# Swap workspaces on two monitors
 import i3
 
-# swap workspaces on two monitors
-for output in i3.get_outputs():
-    if output['active']:
+# collect active outputs
+to_be_swapped = [output for output in i3.get_outputs() if output['active']]
+# only swap when there are two active outputs
+if len(to_be_swapped) == 2:
+    for output in to_be_swapped:
         i3.workspace(output['current_workspace'])
         i3.command('move', 'workspace to output right')
